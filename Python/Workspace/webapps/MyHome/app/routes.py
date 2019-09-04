3bf354d0f493c8ab48b385bb17ef140b354c98f3
Manas Ranjan Mallick <manas1104@gmail.com>
2019-09-04 23:25:56 +1000

    Flask IP

--- Python/Workspace/webapps/MyHome/app/routes.py
+++ Python/Workspace/webapps/MyHome/app/routes.py
@@ -1,5 +1,15 @@
+from flask import render_template
 from app import app
 
+
 @app.route('/')
+@app.route('/index')
 def index():
-  return 'Hello'
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/financials')
+def financials():
+
+    return render_template('financials.html')
