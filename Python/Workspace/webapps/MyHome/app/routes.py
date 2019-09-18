ec78d7197f813c989a41cc37b2a4dbb42e13d051
Manas Ranjan Mallick <manas1104@gmail.com>
2019-09-18 23:24:26 +1000

    latest

--- Python/Workspace/webapps/MyHome/app/routes.py
+++ Python/Workspace/webapps/MyHome/app/routes.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app
+import pandas as pd
 
+
+file=''
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +16,44 @@ def index():
 def financials():
 
     return render_template('financials.html')
+
+
+@app.route('/expenses')
+def expenses():
+
+    return render_template('expenses.html')
+
+
+@app.route('/imgclassify')
+def imgclassify():
+
+    return render_template('imgclassify.html')
+
+
+@app.route('/txtclassify')
+def txtclassify():
+
+    return render_template('txtclassify.html', source='main')
+
+
+@app.route('/upload-file', methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+        if request.files:
+            file = request.files["csvsource"]
+            data = pd.read_excel(file)
+            print(data)
+            print(file.filename)
+            return render_template('txtclassify.html', source='upload', name=file.filename, data=data)
+
+    return render_template('txtclassify.html')
+
+
+@app.route('/classifytext')
+def classifytext():
+
+    print(file)
+    data = pd.read_excel(file)
+    print(data)
+    return render_template('txtclassify.html', source='classify', data=data.to_html())
---
+++ Python/Workspace/webapps/todo/readme.md
@@ -0,0 +1,11 @@
+# Setup
+## Setting up virtual environment
+```
+pip3 install virtualenv
+virtualenv todoapp-venv
+source todoapp-venv/bin/activate
+pip install -r requirements.txt
+```
+
+## App Details
+-
